4f90bbd89ce19adfdf18f4a490313410e996e70c
Guimaraes <69005421+guimaraesdev0@users.noreply.github.com>
2025-01-31 11:17:29 -0300

    Update route.ts

--- frontend/app/api/status/route.ts
+++ frontend/app/api/status/route.ts
@@ -4,7 +4,7 @@ import WebSocket from "ws"; // Usando o WebSocket nativo do Node.js
 export async function GET() {
   return new Promise((resolve) => {
     const socket = new WebSocket("ws://iaseibackend.duckdns.org/socket.io/?EIO=4&transport=websocket");
-
+    // a
     // Quando a conexão for estabelecida, o servidor está online
     socket.on("open", () => {
       resolve(NextResponse.json({ isOnline: true }));
